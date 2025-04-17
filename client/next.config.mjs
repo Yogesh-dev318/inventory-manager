15f6f4c8ed05778a67208c390920304e4458cf43
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-04-17 22:13:38 +0530

    added img

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "inventorymanagements13.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
