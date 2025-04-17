8c3a40d7ac8500863ef9bc0bd2873af75b89b6fa
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-04-17 22:25:06 +0530

    updated small things

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
       {/* RIGHT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
-          <div>
+          {/* <div>
             <button onClick={toggleDarkMode}>
               {isDarkMode ? (
                 <Sun className="cursor-pointer text-gray-500" size={24} />
@@ -63,17 +63,17 @@ const Navbar = () => {
             <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
               3
             </span>
-          </div>
-          <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
+          </div> */}
+          {/* <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" /> */}
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://inventorymanagements13.s3.us-east-1.amazonaws.com/profile.jpg"
+              src="https://inventorymanagements13.s3.us-east-1.amazonaws.com/profile.jpeg"
               alt="Profile"
               width={50}
               height={50}
               className="rounded-full h-full object-cover"
             />
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">Yogesh Choudhary</span>
           </div>
         </div>
         <Link href="/settings">
